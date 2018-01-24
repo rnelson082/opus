83b08edbc2451eef612117556d0f83fdc8bca36a
Ricki Ray Nelson <rickirnelson@gmail.com>
2018-01-23 22:44:20 -0800

    application controller done

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,9 @@
 class Api::UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+  end
+
+  def user_params
+    params.require(:user).permit(:password, :username)
   end
 end
