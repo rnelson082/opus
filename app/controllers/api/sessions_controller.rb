3dc413be1a9ef3f68c289bba1ecd8c5d60ad210c
Ricki Ray Nelson <rickirnelson@gmail.com>
2018-01-24 19:27:58 -0800

    user and session controllers done

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,21 @@
+
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+
+    if @user
+      login(@user)
+      render "api/users/show"
+    else
+      render json: ["error!"], status: 401
+    end
   end
 
   def destroy
+    if logged_in?
+      logout
+    end
+    render json: ["No one logged in"]
   end
+
 end
